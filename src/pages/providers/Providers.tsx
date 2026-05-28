132d0c9044a5cc659152ecab42f2d55eb0d6f2fb
Mayurigore2502 <mayuri25002@gmail.com>
2026-05-28 09:55:27 +0530

    Fixed:Sidebar Scrolls on All Modules Except Dashboard

--- src/pages/providers/Providers.tsx
+++ src/pages/providers/Providers.tsx
@@ -380,7 +380,7 @@ const Providers: React.FC = () => {
   });
 
   return (
-    <div className="flex h-[1024px] overflow-hidden">
+    <div className="flex h-screen overflow-hidden">
       <Sidebar />
       <main className="flex-1 flex flex-col min-w-0 bg-slate-50 overflow-y-auto">
         {/* Topbar */}
